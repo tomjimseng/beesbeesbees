1c08ea10bc8df1ae71ad7f4babce7b6d15c95c24
tomjimseng <tomjimseng@gmail.com>
2020-02-10 11:46:56 -0800

    BeeBeeBee update 2

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  return this.honeyPot += 1;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  if (this.honeyPot > 0) {
+    return this.honeyPot -= 1;
+  }
+}
